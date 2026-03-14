587518b86be04354c902985ede8f6ecf8784d324
Daryna Kazmirchuk <darynakazmirchuk08@gmail.com>
2026-03-14 11:35:40 +0200

    fix: version logger

--- src/main/java/ua/kazmirchuk/OrderProcessorTemplate.java
+++ src/main/java/ua/kazmirchuk/OrderProcessorTemplate.java
@@ -4,23 +4,23 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public abstract class OrderProcessorTemplate {
-    protected static final Logger log = Logger.getLogger(OrderProcessorTemplate.class.getName());
+    private static final Logger logger = LoggerFactory.getLogger(OrderProcessorTemplate.class.getName());
 
     public final void process(Order order, PaymentMethod paymentMethod) {
         try {
-            log.info("Started processing order: " + order.getId());
+            logger.info("Started processing order: " + order.getId());
             validate(order);
             validatePromoCode(order);
             Money total = calculateTotal(order);
             pay(order, total, paymentMethod);
             complete(order);
-            log.info("Order successfully completed: " + order.getId());
+            logger.info("Order successfully completed: " + order.getId());
         } catch (AppException e) {
-            log.warn("Business error during processing: " + e.getMessage());
+            logger.warn("Business error during processing: " + e.getMessage());
             order.setStatus(Order.OrderStatus.FAILED);
             throw e;
         } catch (Exception e) {
-            log.error("Unexpected infrastructure failure");
+            logger.error("Unexpected infrastructure failure");
             order.setStatus(Order.OrderStatus.FAILED);
             throw new AppException("Processing failed due to infrastructure", e);
         }
@@ -31,7 +31,7 @@ public abstract class OrderProcessorTemplate {
     protected abstract Money calculateTotal(Order order);
 
     protected void pay(Order order, Money total, PaymentMethod paymentMethod) throws Exception {
-        log.info("Charging " + total);
+        logger.info("Charging " + total);
         paymentMethod.pay(total);
         order.setStatus(Order.OrderStatus.PAID);
     }
