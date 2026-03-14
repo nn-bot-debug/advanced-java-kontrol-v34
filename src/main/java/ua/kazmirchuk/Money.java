c3eea77c8bdb58a8b0bdb30756ac443d5ceddab1
Daryna Kazmirchuk <darynakazmirchuk08@gmail.com>
2026-03-14 10:42:03 +0200

    feat: add Money

--- src/main/java/ua/kazmirchuk/Money.java
+++ src/main/java/ua/kazmirchuk/Money.java
@@ -1,4 +1,38 @@
 package ua.kazmirchuk;
 
+import java.util.Objects;
+
 public class Money {
+
+    private final double amount;
+    private final String currency;
+
+    public Money(double amount) {
+        this(amount, "UAH");
+    }
+
+    public Money(double amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    public double getAmount() { return amount; }
+    public String getCurrency() { return currency; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Money money = (Money) o;
+        return this.amount == money.amount && Objects.equals(this.currency, money.currency);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount, currency);
+    }
+
+    @Override
+    public String toString() { return amount + " " + currency; }
+
 }
