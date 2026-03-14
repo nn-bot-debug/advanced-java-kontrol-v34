8f2f40908dbb7c2d10f5aa379d69e8b9d57989ec
Daryna Kazmirchuk <darynakazmirchuk08@gmail.com>
2026-03-14 10:29:11 +0200

    feat: add OrderItem

--- src/main/java/ua/kazmirchuk/OrderItem.java
+++ src/main/java/ua/kazmirchuk/OrderItem.java
@@ -1,4 +1,14 @@
 package ua.kazmirchuk;
 
 public class OrderItem {
+    private final String productId;
+    private final Money price;
+
+    public OrderItem(String productId, Money price) {
+        this.productId = productId;
+        this.price = price;
+    }
+
+    public String getProductId() { return productId; }
+    public Money getPrice() { return price; }
 }
